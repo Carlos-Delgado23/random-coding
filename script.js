27122840c3e0e88b24a84527c8cce41fea6d7e26
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-02-16 21:29:20 -0600

    no changes

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ function showSucces(input) {
 function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-
   if (re.test(input.value.trim())) {
     showSucces(input);
   } else {
